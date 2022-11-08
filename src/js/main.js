8b47864e8decb95ca5733537a5cfb8d1e4252150
Demian Bill <demian171@gmail.com>
2022-11-08 12:30:20 +0200

    110801

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,19 @@ if (document.getElementById('slider') != null) {
 }
 
 
+if (document.getElementsByClassName('container') != null) {
+    const accordion = document.getElementsByClassName('container');
+    for (let i = 0; i < accordion.length; i++) {
+        accordion[i].addEventListener('click', function () {
+            this.classList.toggle('active')
+        })
+    }
+}
+
+
 var burgerMenu = document.getElementById('burger-menu');
 var overlay = document.getElementById('menu');
-burgerMenu.addEventListener('click',function(){
+burgerMenu.addEventListener('click', function () {
     this.classList.toggle("close");
     overlay.classList.toggle("overlay");
 });
